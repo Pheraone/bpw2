e7809b6c8f6507b59813c915267eb5b6ab4fa1b9
Pheraone <brechje@me.com>
2020-07-10 09:49:24 +0200

    Base scripts, newer version.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+    // can only be accessed in this class
+    private static GameManager instance;
+
+    // is available from all throughout the code with GameManager.Instance
+    public static GameManager Instance
+    {
+        get
+        {
+            if (instance == null)
+                instance = FindObjectOfType<GameManager>();
+
+            return instance;
+        }
+    }
+
+    // scene references
+    [Header("UI objects")]
+    [SerializeField] internal GameObject mainMenuObject;
+    [SerializeField] internal GameObject pauseObject;
+    
+
+    // state machine
+    internal GameFSM fsm;
+
+    public void Awake()
+    {
+ 
+
+        fsm = new GameFSM();
+        fsm.Initialize();
+    }
+
+    public void Start()
+    {
+        GotoMainMenu();
+    }
+
+    private void Update()
+    {
+        fsm.UpdateState();
+    }
+
+
+    public void StartLevel()
+    {
+        
+    }
+
+    public void EndLevel()
+    {
+        
+    }
+
+    #region GOTO_STATE
+    public void GotoMainMenu()
+    {
+        fsm.GotoState(GameStateType.MainMenu);
+        EndLevel();
+    }
+
+    public void GotoPlay()
+    {
+        fsm.GotoState(GameStateType.Play);
+    }
+
+    public void GotoPause()
+    {
+        fsm.GotoState(GameStateType.Pause);
+    }
+    #endregion
+}
---
+++ Assets/Scripts/GameState.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public enum GameStateType { MainMenu, Play, Pause }
+
+public abstract class GameState
+{
+    public abstract void Enter();
+    public abstract void Exit();
+    public abstract void Update();
+}
+
+public class MainMenuState : GameState
+{
+    public override void Enter()
+    {
+        GameManager.Instance.mainMenuObject.SetActive(true);
+        Cursor.visible = false;
+        Time.timeScale = 0;
+    }
+
+    public override void Exit()
+    {
+        GameManager.Instance.mainMenuObject.SetActive(false);
+        Time.timeScale = 1;
+    }
+
+    public override void Update()
+    {
+        if (Input.anyKeyDown)
+        {
+            GameManager.Instance.fsm.GotoState(GameStateType.Play);
+            GameManager.Instance.StartLevel();
+        }
+    }
+}
+
+public class PlayState : GameState
+{
+    public override void Enter()
+    {
+        
+    }
+
+    public override void Exit()
+    {
+        
+    }
+
+    public override void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+            GameManager.Instance.fsm.GotoState(GameStateType.Pause);
+    }
+}
+
+public class PauseState : GameState
+{
+    public override void Enter()
+    {
+        GameManager.Instance.pauseObject.SetActive(true);
+        Time.timeScale = 0;
+        Cursor.visible = true;
+    }
+
+    public override void Exit()
+    {
+        GameManager.Instance.pauseObject.SetActive(false);
+        Time.timeScale = 1;
+    }
+
+    public override void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+            GameManager.Instance.fsm.GotoState(GameStateType.Play);
+    }
+}
