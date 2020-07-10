a3ce577380991427dd9a5def9d6a4bded076174c
Pheraone <brechje@me.com>
2020-07-10 23:53:51 +0200

    TextMesh, new scripts, not finished.

---
+++ Assets/Scripts/Generation/Chamber.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chamber
+{
+    public Vector2 gridPosition;
+    //If more make string array?
+    public int type;
+    public bool doorTop, doorBot, doorLeft, doorRight;
+
+    public Chamber(Vector2 gridPosition, int type)
+    {
+        this.gridPosition = gridPosition;
+        this.type = type;
+    }
+}
---
+++ Assets/Scripts/Generation/LevelGeneration.cs
@@ -0,0 +1,261 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGeneration : MonoBehaviour
+{
+    Vector2 worldSize = new Vector2(4, 4);
+    Chamber[,] chambers;
+    List<Vector2> takenPositions = new List<Vector2>();
+    int gridSizeX, gridSizeY, numberOfChambers = 20;
+    public GameObject roomObj;
+
+    void Start()
+    {
+        //math for generation
+        if(numberOfChambers >= (worldSize.x * 2) * (worldSize.y * 2))
+        {
+            numberOfChambers = Mathf.RoundToInt((worldSize.x * 2) * (worldSize.y * 2));
+        }
+        gridSizeX = Mathf.RoundToInt(worldSize.x);
+        gridSizeY = Mathf.RoundToInt(worldSize.y);
+
+        CreateChambers();
+        SetChamberDoors();
+        DrawMap();
+    }
+
+    public void CreateChambers()
+    {
+        //setup
+        chambers = new Chamber[gridSizeX * 2, gridSizeX * 2];
+        //starting point at the center of the scene, type 1
+        chambers[gridSizeX, gridSizeY] = new Chamber(Vector2.zero, 1);
+        takenPositions.Insert(0, Vector2.zero);
+        Vector2 checkPosition = Vector2.zero;
+
+        float randomCompare = 0.2f;
+        float randomCompareBegin = 0.2f;
+        float randomCompareEnd = 0.2f;
+
+        //loop for rooms
+        for(int i = 0; i < numberOfChambers; i++)
+        {
+            float randomPerc = ((float)i) / (((float)numberOfChambers - 1));
+            //magic numbers? 
+            randomCompare = Mathf.Lerp(randomCompareBegin, randomCompareEnd, randomPerc);
+            //get new position
+            checkPosition = NewPosition();
+            //test new position //more interesting shapes | change this to change whole shape dungeon
+            if(NumberOfNeighbours(checkPosition, takenPositions) > 1 && Random.value > randomCompare)
+            {
+                int iterations = 0;
+                do
+                {
+                    checkPosition = SelectiveNewPosition();
+                    iterations++;
+                } while (NumberOfNeighbours(checkPosition, takenPositions) > 1 && iterations < 100);
+                if(iterations >= 50)
+                { 
+                    Debug.Log("Can not create with fewer neighbours than : " + NumberOfNeighbours(checkPosition, takenPositions));
+                }
+                //finalize position
+                chambers[(int)checkPosition.x + gridSizeX, (int)checkPosition.y + gridSizeY] = new Chamber(checkPosition, 0);
+                takenPositions.Insert(0, checkPosition);
+            }
+        }
+
+    }
+
+    Vector2 NewPosition()
+    {
+        int x = 0;
+        int y = 0;
+        Vector2 checkingPosition = Vector2.zero;
+
+        do
+        {
+            //take randomly selected position from list
+            int index = Mathf.RoundToInt(Random.value * (takenPositions.Count - 1));
+            x = (int)takenPositions[index].x;
+            y = (int)takenPositions[index].y;
+
+            //up? down? left? right?
+            bool UpDown = (Random.value < 0.5f);
+            bool positive = (Random.value < 0.5f);
+
+            if (UpDown)
+            {
+                if (positive)
+                {
+                    y += 1;
+                }
+                else
+                {
+                    y -= 1;
+                }
+            }
+            else
+            {
+                if (positive)
+                {
+                    x += 1;
+                }
+                else
+                {
+                    x -= 1;
+                }
+            }
+            //go through loop till free position is found within grid
+            checkingPosition = new Vector2(x, y);
+        } while(takenPositions.Contains(checkingPosition) || x >= gridSizeX || x <= -gridSizeX || y >= gridSizeY || y <= - gridSizeY);
+
+        return checkingPosition;
+    }
+
+    //gets chamber with 1 neighbour for extra brancing effect
+    Vector2 SelectiveNewPosition()
+    {
+        //Ugly if's, need to change this if I can
+        int index = 0;
+        int inc;
+        int x = 0;
+        int y = 0;
+        Vector2 checkingPosition = Vector2.zero;
+
+        do
+        {
+            inc = 0;
+            do
+            {
+                //take randomly selected position from list
+                index = Mathf.RoundToInt(Random.value * (takenPositions.Count - 1));
+                inc++;
+            } while (NumberOfNeighbours(takenPositions[index], takenPositions) > 1 && inc < 100);
+
+            x = (int)takenPositions[index].x;
+            y = (int)takenPositions[index].y;
+
+            //up? down? left? right?
+            bool upDown = (Random.value < 0.5f);
+            bool positive = (Random.value < 0.5f);
+
+            if (upDown)
+            {
+                if (positive)
+                {
+                    y += 1;
+                }
+                else
+                {
+                    y -= 1;
+                }
+            }
+            else
+            {
+                if (positive)
+                {
+                    x += 1;
+                }
+                else
+                {
+                    x -= 1;
+                }
+            }
+
+            //go through loop till free position is found within grid
+            checkingPosition = new Vector2(x, y);
+        } while (takenPositions.Contains(checkingPosition) || x >= gridSizeX || x <= -gridSizeX || y >= gridSizeY || y <= -gridSizeY);
+
+        if (inc >= 100)
+        {
+            Debug.Log("Error: could not find position with only 1 neighbour");
+        }
+
+        return checkingPosition;
+    }
+
+    int NumberOfNeighbours(Vector2 checkingPosition, List<Vector2> usedPositions)
+    {
+        int ret = 0;
+        //question: does this do the same as...
+        if(usedPositions.Contains(checkingPosition + Vector2.right))
+        {
+            ret++;
+        }
+        if (usedPositions.Contains(checkingPosition + Vector2.left))
+        {
+            ret++;
+        }
+        if (usedPositions.Contains(checkingPosition + Vector2.up))
+        {
+            ret++;
+        }
+        if (usedPositions.Contains(checkingPosition + Vector2.down))
+        {
+            ret++;
+        }
+        //...dit (wat is handiger en waarom?)
+        /*  if(usedPositions.Contains(checkingPosition+ Vector2.right) || usedPositions.Contains(checkingPosition + Vector2.left) || usedPositions.Contains(checkingPosition + Vector2.up) || usedPositions.Contains(checkingPosition + Vector2.down))
+        {
+            ret++;
+        }*/
+        return ret;
+
+    }
+
+    public void SetChamberDoors()
+    {
+        for(int x = 0; x<((gridSizeX * 2)); x++)
+        {
+            for(int y = 0; y < ((gridSizeY * 2)); y++)
+            {
+                if (chambers[x,y] == null)
+                {
+                    continue;
+                }
+                Vector2 gridPosition = new Vector2(x, y);
+                if (y - 1< 0)
+                {
+                    chambers[x, y].doorBot = false;
+                } 
+                else
+                {
+                    chambers[x, y].doorBot = (chambers[x, y - 1] != null);
+                }
+
+                if (y - 1< 0)
+                {
+                    chambers[x, y].doorTop = false;
+                } 
+                else
+                {
+                    chambers[x, y].doorTop = (chambers[x, y - 1] != null);
+                }
+
+                if (x - 1 < 0)
+                {
+                    chambers[x, y].doorLeft = false;
+                }
+                else
+                {
+                    chambers[x, y].doorLeft = (chambers[x, y - 1] != null);
+                }
+                if (x - 1 < 0)
+                {
+                    chambers[x, y].doorRight = false;
+                }
+                else
+                {
+                    chambers[x, y].doorRight = (chambers[x, y - 1] != null);
+                }
+            }
+        }
+
+    }
+
+    public void DrawMap()
+    {
+
+    }
+}
