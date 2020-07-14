70305d8d2c80a3ece7fc8098227c14b85b6cd16d
Pheraone <brechje@me.com>
2020-07-14 13:27:29 +0200

    Player be moving

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float moveSpeed = 10f;
+    public Rigidbody2D rb;
+
+    Vector2 movement;
+
+    void Update()
+    {
+        //input
+        movement.x  = Input.GetAxisRaw("Horizontal");
+        movement.y  = Input.GetAxisRaw("Vertical");
+    }
+
+    //50 times a second
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+}
